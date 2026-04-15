f7495b99355d40080823aea4e82f9292fef1eb44
codeparce <codeparcelts@gmail.com>
2026-04-15 14:55:46 -0500

    DEVOPS : NEW: Creacion de tablas y configuracion

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
-import { PrismaClient } from '@prisma/client/extension';
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
 
 
 dotenv.config();
@@ -29,15 +22,7 @@ app.all('/api/:service/*path', async (req, res) => {
     const { service } = req.params;
     const splat = req.params.path;
     console.log(`Received request for service: ${service}, path: ${splat.join('/')}`);
-    const routes = {
-        users: 'http://localhost:3001',
-        orders: 'http://localhost:3002',
-        products: 'http://localhost:3003'
-    };
 
-    const services = await prisma.service.findUnique({
-        where: { name: service}
-    });
 
     return res.status(200).json({ message: `Request received for service: ${service} with path: ${splat}` });
     //   const target = routes[service];
