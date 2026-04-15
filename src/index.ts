b2d7b1df91b280685892f1bad735af2a5ba1b937
codeparce <codeparcelts@gmail.com>
2026-04-15 17:00:28 -0500

    DEVOPS : FIX : Correcion de carpetas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import { prisma } from "./lib/prisma";
 
 
 dotenv.config();
@@ -13,33 +14,66 @@ app.use(morgan(process.env.ENVIROMENT || 'dev'));
 app.use(express.json());
 
 
-
 app.get('/', (req, res) => {
     res.json({ status: 'ok' });
 });
 
+app.all('/api/:service', async (req, res) => {
+    const { service } = req.params;
+
+    try {
+
+        const data = await getPrismaData(service);
+        const targetUrl = `${data[0]?.url}/${service}/`;
+        
+        const response = await fetch(targetUrl, {
+            method: req.method,
+            body: JSON.stringify(req.body)
+        });
+
+        const result = await response.json();
+
+        return res.status(response.status).json(result);
+
+    } catch (error : any) {
+        return res.status(500).json({ error: error.message || 'Internal Server Error' });
+    }
+
+
+});
+
 app.all('/api/:service/*path', async (req, res) => {
     const { service } = req.params;
-    const splat = req.params.path;
-    console.log(`Received request for service: ${service}, path: ${splat.join('/')}`);
+    const splat = req.params["path"];
 
+    try {
 
-    return res.status(200).json({ message: `Request received for service: ${service} with path: ${splat}` });
-    //   const target = routes[service];
-    //   if (!target) return res.status(404).json({ error: 'Service not found' });
+        const data = await getPrismaData(service);
+        const targetUrl = `${data[0]?.url}/${service}/${splat}`;
+        
+        const response = await fetch(targetUrl, {
+            method: req.method,
+            body: JSON.stringify(req.body)
+        });
 
-    //   try {
-    //     const response = await axios({
-    //       method: req.method,
-    //       url: `${target}${req.url.replace(`/api/${service}`, '')}`,
-    //       data: req.body,
-    //       headers: { ...req.headers, host: undefined }
-    //     });
-    //     res.status(response.status).json(response.data);
-    //   } catch (error) {
-    //     res.status(error.response?.status || 500).json(error.response?.data || { error: error.message });
-    //   }
+        const result = await response.json();
+
+        return res.status(response.status).json(result);
+        
+    } catch (error: any) {
+        return res.status(500).json({ error: error.message || 'Internal Server Error' });
+    }
+
 });
+
+const getPrismaData = async (service: string) => {
+    const data = await prisma.service.findMany({
+        where: {
+            name: service
+        }
+    });
+    return data;
+}
 
 app.listen(PORT, () => {
     console.log(`Gateway running on  ${process.env.GATEWAY_URL}`);
