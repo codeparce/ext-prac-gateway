f674994d256f07ad6a3dd1c2caf1685ae307092f
codeparce <codeparcelts@gmail.com>
2026-04-17 11:36:03 -0500

    DEVOPS : FIX : Correcion de cors del gateway

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,16 @@ const PORT = process.env.PORT || 8080;
 app.use(morgan(process.env.ENVIROMENT || 'dev'));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 
 app.get('/', (req, res) => {
     res.json({ status: 'ok' });
